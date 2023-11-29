0eacba4581e3739f7b18eb2038ec33c1b8eb0b89
Angga Rofiul Putra <optimistic.eth@gmail.com>
2023-11-29 10:12:55 +0700

    Update kegiatan1.py

--- kegiatan1.py
+++ kegiatan1.py
@@ -2,13 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Kegiatan 1
-# xpoints = np.array([1,3])
-# ypoints = np.array([8,10])
+xpoints = np.array([1,3])
+ypoints = np.array([8,10])
 
-# plt.plot(xpoints, ypoints)
-# plt.show()
-
-#kegiatan2
-
-
-#kegiatan3
+plt.plot(xpoints, ypoints)
+plt.show()
