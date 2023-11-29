bb019b3ad1485fbc88ea25587fb83a49ff46a983
AaveVenus <optimistic.eth@gmail.com>
2023-11-29 10:10:51 +0700

    codelab

---
+++ kegiatan1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Kegiatan 1
+# xpoints = np.array([1,3])
+# ypoints = np.array([8,10])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+#kegiatan2
+
+
+#kegiatan3
---
+++ kegiatan4.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [8, 4, 9, 1, 5]
+y = [5, 11, 3, 6, 10]
+
+plt.scatter(x, y)
+plt.show()
